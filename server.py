ea5afa5abd2852dd5a3e059b68250312409f25f0
Ubuntu <jlott@jlott-dev.c1c4scrw32mebn0hgkspwtij5g.bx.internal.cloudapp.net>
2019-10-21 03:37:54 +0000

    server.py passes pylint

--- server.py
+++ server.py
@@ -1,42 +1,46 @@
+"""Classes to represent the various ways of running nethack, mostly either
+   a local subprocess or (TODO) a remote ssh connection."""
+
 import os.path
 import pty
 import select
 import subprocess
 
 def default_subprocess():
+    """Run a local copy of nethack from the default install location"""
     return SubprocessNethack(SubprocessNethack.DEFAULT_NH)
 
 class SubprocessNethack:
+    """Represents interacting with nethack running in a local subprocess"""
 
     DEFAULT_NH = os.path.expanduser('~/nh/install/games/nethack')
 
     def __init__(self, *args):
+        """Run a nethack subprocess using the given command"""
         (parent_pty, child_pty) = pty.openpty()
         self.input_pty = open(parent_pty, 'wb', buffering=0)
         self.subproc = subprocess.Popen(args,
-            stdin=child_pty,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            bufsize=0, # unbuffered
-            close_fds=True)
+                                        stdin=child_pty,
+                                        stdout=subprocess.PIPE,
+                                        bufsize=0, # unbuffered
+                                        close_fds=True)
 
     def read(self, timeout=1.0):
-        return self._handleOut(self.subproc.stdout, timeout)
-
-    def error(self, timeout=1.0):
-        return self._handleOut(self.subproc.stderr, timeout)
-
-    def _handleOut(self, stream, timeout):
-        (readable, _, _) = select.select((stream,), (), (), timeout)
+        """Read data from nethack stdout if available, or if no data is
+           available by the given timeout, return None"""
+        (readable, _, _) = select.select((self.subproc.stdout,), (), (),
+                                         timeout)
         if readable:
-            return stream.read(1024)
+            return self.subproc.stdout.read(1024)
+        return None
 
     def write(self, bytes_):
+        """Write data to nethack stdin"""
         self.input_pty.write(bytes_)
 
-    # this is mostly for the tests
     def terminate(self):
+        """Immediately terminate the nethack subprocess with SIGTERM. This
+           is intended to be used from tests."""
         self.input_pty.close()
         self.subproc.terminate()
         self.subproc.communicate()
-
